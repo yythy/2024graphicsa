10dcb80015cd75d0536e33a5775dbd5e368a438c
yythy <114201409+yythy@users.noreply.github.com>
2024-02-22 16:33:08 +0800

    week01

---
+++ week01_GLUT_10_lines/main.cpp
@@ -0,0 +1,15 @@
+#include <GL/glut.h>
+void display()
+{
+    glutSolidTeapot( 0.3 );
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("GLUT Shapes");
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}
