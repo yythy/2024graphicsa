232a38e3a90e4fac6f936c58e12267f91fcffd25
yythy <114201409+yythy@users.noreply.github.com>
2024-04-11 16:57:06 +0800

    Add files via upload

---
+++ week08-1_glm_gundam/main.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+void drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/Gundam.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    drawmodel();
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week08 gundam");
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}
