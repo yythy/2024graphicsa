dba144b52dc78aadf8d8503c63ee61d722b04a38
yythy <114201409+yythy@users.noreply.github.com>
2024-04-18 16:48:53 +0800

    Add files via upload

---
+++ week09/week09-2_glutSoildTeapot_glutSoildSphere/main.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    ///glutSolidCube( 0.3 );
+    ///glutSolidSphere( 0.3,30,30 );
+    glPushMatrix();
+        glTranslatef(-0.5,+0.3,0);
+        glutSolidTeapot(0.3);
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(+0.0,+0.3,0);
+        glutSolidCube(0.3);
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(+0.5,+0.3,0);
+        glutSolidSphere(0.3,30,30);
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("GLUT Shapes");
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}
